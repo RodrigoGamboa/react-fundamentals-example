39765794ca20535d0631ba088147cfbbf2e372aa
Rodrigo Gamboa <rodrigogamboatorres@gmail.com>
2023-03-27 08:07:10 -0600

    feat: add Home page and basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom'
 import Example1 from './pages/Example1'
+import Example1Solved from './pages/Example1Solved'
+import Home from './pages/Home'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Example1 />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/example1' element={<Example1 />} />
+        <Route path='/example1-solved' element={<Example1Solved />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+function Home() {
+  return (
+    <h1>asdf</h1>
+  )
+} 
+
+export default Home
