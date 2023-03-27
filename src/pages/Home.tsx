297006defa3efc1c7f80d312216b3c62d684e83b
Rodrigo Gamboa <rodrigogamboatorres@gmail.com>
2023-03-27 08:48:59 -0600

    fix: add styles, routing from the home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,16 @@
+import { Link } from "react-router-dom"
+
 function Home() {
   return (
-    <h1>asdf</h1>
+    <main style={{height: "100vh", display: "flex", flexDirection: "column", justifyContent: "center"}}>
+      <h1>React Fundamentals</h1>
+      <h2>Recreate the functionality of this example:</h2>
+      <div>
+        <Link to="/example1-solved">Example</Link> 
+        <Link to="/example1">Exercise</Link> 
+      </div>
+      
+    </main>
   )
 } 
 
