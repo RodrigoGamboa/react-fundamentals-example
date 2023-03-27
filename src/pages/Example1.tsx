d17fb072698e53a24031d61f508cb98ac114cd09
Rodrigo Gamboa <rodrigogamboatorres@gmail.com>
2023-03-27 08:00:37 -0600

    fix: fix example1 indentation

--- src/pages/Example1.tsx
+++ src/pages/Example1.tsx
@@ -6,31 +6,31 @@ function Example1() {
 
   useEffect(() => {
     getAllPokemon(10)
-        .then(data => setPokemon(data))
+      .then(data => setPokemon(data))
   }, []);
 
   return (
     <>
 	    <div style={{position: "sticky", top: "30px", display: "flex", justifyContent: "center", gap: "1rem"}}>
-        	    <input style={{border: "3px solid black", marginBottom: "2rem", height: "2rem"}} placeholder="Search Pokemon" />
+        <input style={{border: "3px solid black", marginBottom: "2rem", height: "2rem"}} placeholder="Search Pokemon" />
 	    </div>
-            <section style={{display: "grid", gridTemplateColumns: "1fr 1fr 1fr", gap: "1rem", marginBottom: "4rem"}}>
-        	    {pokemon.length !== 0 && pokemon.map((pokemon: any) => 
-        	        <div 
+      <section style={{display: "grid", gridTemplateColumns: "1fr 1fr 1fr", gap: "1rem", marginBottom: "4rem"}}>
+        {pokemon.length !== 0 && pokemon.map((pokemon: any) => 
+          <div 
 	    	    key={pokemon.name}
 	    	    style={{display: "flex", flexDirection: "column", alignItems: "center", border: "3px solid black", padding: "1rem", width: "350px"}}
-	    	>
-        	            <h1 style={{margin: "0.5rem"}}>{pokemon.name}</h1>
-        	            <img src={pokemon.sprites.front_default} />
+	    	  >
+        	  <h1 style={{margin: "0.5rem"}}>{pokemon.name}</h1>
+        	  <img src={pokemon.sprites.front_default} />
 	    	    <h2 style={{margin: "0.25rem"}}>${pokemon.price}</h2>
-        	            <button style={{marginBottom: "0.25rem", background: ` ? 'IndianRed' : 'none'}`}}> &hearts;</button>
-        	            <button>Agregar</button>
-        	        </div>
-        	    )}
-        	</section>
+        	  <button style={{marginBottom: "0.25rem", background: ` ? 'IndianRed' : 'none'}`}}> &hearts;</button>
+        	  <button>Agregar</button>
+        	</div>
+        )}
+      </section>
 	    <aside style={{position: "sticky", bottom: "50px", display: "flex", justifyContent: "center", gap: "1rem"}}>
-	        <button style={{background: `? 'LightSkyBlue' : 'white'`}}>0 items (total: $0)</button>
-	        <button>Clear</button>
+	      <button style={{background: `? 'LightSkyBlue' : 'white'`}}>0 items (total: $0)</button>
+	      <button>Clear</button>
 	    </aside>
     </>
   )
